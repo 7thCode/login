38537afee57cecaf39f7b47cbd33b16e7db5a560
MIKIO ODA <oda.mikio@gmail.com>
2020-11-23 11:53:11 +0900

    5

--- app.js
+++ app.js
@@ -4,87 +4,105 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+// const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
 
 
+/*
+ 	Mongoose
 
-// Mongoose
+ 	MongoDBのObjectアダプター
 
-const mongoose = require('mongoose');									              // Mongoose MongoDB Objectアダプター
+	MongoDBのカプセル化。(抽象レイヤ)
+
+*/
+
+const mongoose = require('mongoose');
 
 // MongoDB接続時、一度だけ実行されるハンドラ
 // データベースに接続されていることを前提とする処理はこの中に。
 mongoose.connection.once("open", () => {
 
+	/*
+		Session
 
+		Expressのクッキーセッションをデータベースで永続化
 
-	// Session
+	*/
 	// --------------------ここから--------------------
 
-	const SESSION_MODULE = require('express-session');						    // Express Session
-	const MongoStore = require("connect-mongo")(SESSION_MODULE);      // 暗号化されたクッキーとデータベースに保存されたセッションを関連づける
+	const SESSION_MODULE = require('express-session');					// Express Session
+	const MongoStore = require("connect-mongo")(SESSION_MODULE);		// 暗号化されたクッキーとデータベースに保存されたセッションを関連づける
 
-	app.use(SESSION_MODULE({						                              // sessionとMongoDBの接続
-		name: "login",	                                            // セッション名
-		secret: "hogehoge",	                                        // セッション暗号化キー
-		resave: false,		                                          //
+	app.use(SESSION_MODULE({											// sessionとMongoDBの接続
+		name: "login",	                                           		// セッション名
+		secret: "hogehoge",												// セッション暗号化キー
+		resave: false,													//
 		rolling: true,		                                       		//
-		saveUninitialized: true,                                    //
-		cookie: {maxAge: 365 * 24 * 60 * 60 * 1000},                //クッキー側設定
-		store: new MongoStore({											                // MongoDB側接続オブジェクト
-			mongooseConnection: mongoose.connection,                  // Mongoose接続
+		saveUninitialized: true,										//
+		cookie: {maxAge: 365 * 24 * 60 * 60 * 1000},					//クッキー側設定
+		store: new MongoStore({											// MongoDB側接続オブジェクト
+			mongooseConnection: mongoose.connection,					// Mongoose接続
 			ttl: 365 * 24 * 60 * 60,
 			clear_interval: 60 * 60,
 		}),
-  }));
+  	}));
 
 	// --------------------ここまで--------------------
 
 
 
-	// Passport
+
+	/*
+		Passport
+
+		認証モジュール
+		アカウントレコード作成、パスワード認証/ハッシュ化などを行う
+
+	*/
 	// --------------------ここから--------------------
 
-	const PASSPORT_MODULE = require('passport');									      // Passport 認証用モジュール
-	const LocalStrategy = require('passport-local').Strategy;				    // パスワード認証用Passportプラグイン
-  const LocalAccount = require("./model/account");						        // Mongooseアカウント定義
+	const PASSPORT_MODULE = require('passport');						// Passport 認証用モジュール
+	const LocalStrategy = require('passport-local').Strategy;			// パスワード認証用Passportプラグイン
+	const LocalAccount = require("./model/account");					// Mongooseアカウント定義
 
-  PASSPORT_MODULE.serializeUser((user, done) => {done(null, user)});
+	PASSPORT_MODULE.serializeUser((user, done) => {done(null, user)});
 
 	PASSPORT_MODULE.deserializeUser((user, done) => {done(null, user)});
 
-  PASSPORT_MODULE.use(new LocalStrategy(LocalAccount.authenticate()));// Mongooseアカウント定義 - パスワード認証
+	PASSPORT_MODULE.use(new LocalStrategy(LocalAccount.authenticate()));	// Mongooseアカウント定義 - パスワード認証
 
-	app.use(PASSPORT_MODULE.initialize());									            // Passportの使用前に必須
-  app.use(PASSPORT_MODULE.session());										              // Passportの使用前に必須
+	app.use(PASSPORT_MODULE.initialize());								// Passportの使用前に必須
+	app.use(PASSPORT_MODULE.session());									// Passportの使用前に必須
 
 	// --------------------ここまで--------------------
 
 
+	/*
+		Application
 
-	// Application
+	*/
 	// --------------------ここから--------------------
 
 	// ログイン要求
@@ -187,7 +205,7 @@ const options = {
 	useUnifiedTopology: true,
 };
 
-const connect_url = "mongodb://localhost/login";						// MongoDB接続先
+const connect_url = "mongodb://localhost/login";			// MongoDB接続先
 
 mongoose.connect(connect_url, options).catch((error) => {   // Mongo接続
 
