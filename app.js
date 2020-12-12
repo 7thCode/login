c8013d722fc861e9c7c8012534b23b0382a3368e
MIKIO ODA <oda.mikio@gmail.com>
2020-12-12 10:04:10 +0900

    15

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'jade');
 // app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser());≠
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
