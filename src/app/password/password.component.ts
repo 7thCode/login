592e616f3f5489c4839f497a5b51951293ce657c
MIKIO ODA <oda.mikio@gmail.com>
2021-09-11 17:14:11 +0900

    ...

--- src/app/password/password.component.ts
+++ src/app/password/password.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {TweetService} from '../tweet/tweet.service';
 import {PasswordService} from './password.service';
 
 @Component({
@@ -11,17 +10,7 @@ export class PasswordComponent implements OnInit {
 
 	public password: string = '';
 	public strength: number;
-
-	public sec: number = 0;
-	public min: number = 0;
-	public hour: number = 0;
-	public days: number = 0;
-	public mon: number = 0;
-	public year: number = 0;
-	public myear: number = 0;
-	public byear: number = 0;
-	public tyear: number = 0;
-	public qyear: number = 0;
+	public unit: string;
 
 	constructor(public passwordService: PasswordService) {
 	}
@@ -30,25 +19,36 @@ export class PasswordComponent implements OnInit {
 	}
 
 	public Check(): void {
+
+		const units: any[] = [
+			[1000, "秒"],
+			[1000 * 60, "分"],
+			[1000 * 60 * 60, "時間"],
+			[1000 * 60 * 60 * 24, "日"],
+			[1000 * 60 * 60 * 24 * 30, "か月"],
+			[1000 * 60 * 60 * 24 * 30 * 12, "年"],
+			[1000 * 60 * 60 * 24 * 30 * 12 * 10000, "万年"],
+			[1000 * 60 * 60 * 24 * 30 * 12 * 10000 * 10000, "億年"],
+			[1000 * 60 * 60 * 24 * 30 * 12 * 10000 * 10000 * 10000, "兆年"],
+			[1000 * 60 * 60 * 24 * 30 * 12 * 10000 * 10000 * 10000 * 10000, "京年"]
+		]
+
 		this.passwordService.strength(this.password, (error, result) => {
-			this.strength = result;
-			this.sec = Math.floor(this.strength / 1000);
-			this.min  = Math.floor(this.sec / 60)
-			this.hour =  Math.floor(this.min / 60)
-			this.days =  Math.floor(this.hour / 24)
-			this.mon =  Math.floor(this.days / 30)
-			this.year =  Math.floor(this.mon / 12)
-			this.myear =  Math.floor(this.year / 10000)
-			this.byear =  Math.floor(this.myear / 10000)
-			this.tyear =  Math.floor(this.byear / 10000)
-			this.qyear =  Math.floor(this.tyear / 10000)
+			if (!error) {
+				let strength: number = 0;
+				let unit: string = "";
 
+				let digit:number = 0;
+				while (Math.floor(result / units[digit][0])) {
+					strength = Math.floor(result / units[digit][0]);
+					unit = units[digit][1];
+					digit++;
+				}
 
-
+				this.strength = strength;
+				this.unit = unit;
+			}
 		});
 	}
-
-
-
 
 }
